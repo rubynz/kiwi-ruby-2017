c94a251e76c8a272d80ea3d38a5fffc65fc0f0b5
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-08-26 16:18:38 +1200

    Remove jQuery and refactor the mobile nav.

--- source/assets/public/index.js
+++ source/assets/public/index.js
@@ -1,33 +1,21 @@
-/**
- * JavaScript goes here
- */
-$(document).ready(function () {
-  mobileNav();
+var body = document.body;
+var mobileNavContainer = document.querySelector('.nav--mobile');
+var mobileNavTrigger = document.querySelector('.nav--mobile__show');
+
+function bodyClickHandler(event) {
+  var trigger = event.target;
+  if (trigger == mobileNavTrigger) {return;}
+  removeClass();
+}
+function addClass () {
+  mobileNavContainer.classList.add('active');
+  body.addEventListener('click', bodyClickHandler);
+}
+function removeClass () {
+  mobileNavContainer.classList.remove('active');
+  body.removeEventListener('click', bodyClickHandler);
+}
+
+mobileNavTrigger.addEventListener('click', function(){
+  mobileNavContainer.classList.contains('active') ? removeClass() : addClass();
 });
-
-function hide ($element) {
-  $($element).hide(200);
-  $($element).removeClass('active');
-}
-
-function mobileNav () {
-  var $mobileNav = $('.nav--mobile');
-
-  $('.nav--mobile__show').click(function () {
-    if ( $mobileNav.hasClass('active') ) {
-      hide($mobileNav);
-      return;
-    }
-
-    $mobileNav.addClass('active');
-    $mobileNav.show(200);
-  });
-
-  $('body').click(function (event) {
-    if (!$mobileNav.hasClass('active')) { return; }
-    if ($(event.target).is('.nav--mobile__show') || $(event.target).parent().is('.nav--mobile__show')) { return; }
-
-    $($mobileNav).removeClass('active');
-    hide($mobileNav);
-  });
-}
