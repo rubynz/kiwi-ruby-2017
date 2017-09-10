3614a4d371142f36f8022a5dc923f13e9a8317c6
Shaun O'Connell <shaun.oconnell@abletech.co.nz>
2017-09-10 23:57:22 +1200

    Fix a typo in closeMenu

--- source/assets/public/index.js
+++ source/assets/public/index.js
@@ -9,18 +9,18 @@ var mobileNavTrigger = document.querySelector('.nav--mobile__show');
 function bodyClickHandler(event) {
   var trigger = event.target;
   if (trigger == mobileNavTrigger) {return;}
-  closeMneu();
+  closeMenu();
 }
 function openMenu () {
   mobileNavContainer.classList.add('active');
   body.addEventListener('click', bodyClickHandler);
 }
-function closeMneu () {
+function closeMenu () {
   mobileNavContainer.classList.remove('active');
   body.removeEventListener('click', bodyClickHandler);
 }
 
 mobileNavTrigger.addEventListener('click', function(e){
   e.preventDefault();
-  mobileNavContainer.classList.contains('active') ? closeMneu() : openMenu();
+  mobileNavContainer.classList.contains('active') ? closeMenu() : openMenu();
 });
